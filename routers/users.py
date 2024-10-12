6265e4b62d5edbbac3cc37d44c762e571051e8da
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-10-12 20:54:43 +0200

    [FEAT] username and password login with jwt token response

--- routers/users.py
+++ routers/users.py
@@ -12,13 +12,12 @@ from jwt.exceptions import InvalidTokenError
 
 from passlib.context import CryptContext
 
-
+from config import SECRET_KEY
 
 
 #### SETTINGS ####
 
 # jwt settings
-SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -33,7 +32,7 @@ fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
+        "password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
     }
 }
 
@@ -79,7 +78,54 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
+# gets the user with the specified username from the database and returns it with the password
+def get_user(db, username: str) -> WithPasswordUser:
+    """DO NOT USE THE RETURN OF THIS FUNCTION AS RESPONSE TO A USER REQUEST (PASSWORD EXPOSED). Currently checks inside the dictionary for a username match, next it will use a database"""
+    if username in db:
+        user_dict = db[username]
+        return WithPasswordUser(**user_dict)
 
+# gets the user by username and checks if username and password are right and returns the user with the password
+def authenticate_user(fake_db, username: str, password: str):
+    """USER WITH PASSWORD EXPOSED. Checks username (using get_user()) and password"""
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+# token generation
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """data wants a {"sub": username} dict"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# gets the user from token by decoding the token and using the decoded username to find the right user
+async def get_user_from_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError: 
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 #### END FUNCTIONS ####
 
@@ -88,9 +134,19 @@ def verify_password(plain_password: str, hashed_password: str):
 ##### ROUTES ####
 
 # post route to authenticate using email and password to get a valid token
-@router.post('/get-token')
-async def get_token():
-    return "post route to authenticate using email and password to get a valid token"
+@router.post('/token')
+async def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    print("ok")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    new_access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return Token(access_token=new_access_token, token_type="bearer")
 
 # post route to create a new user with email and password
 @router.post('/register')
